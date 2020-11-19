5689403c5b6e7309bbca8f8a90bc334738df5334
Kerri Ryan <kerri.ryan11@gmail.com>
2020-11-19 15:52:57 -0500

    third commit

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const {db, Page, User} = require('./models');
-const morgan = require('morgan');
+const express = require("express");
+const { db, Page, User } = require("./models");
+const morgan = require("morgan");
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.send('hello world'); 
-})
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
 
-const init = async () => {
-    await Page.sync();
-    await User.sync();
-    // make sure that you have a PORT constant
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}!`);
-    });
-}
-  
-init();
-  
+// const init = async () => {
+//     await Page.sync();
+//     await User.sync();
+//     // make sure that you have a PORT constant
+//     // app.listen(PORT, () => {
+//     //   console.log(`Server is listening on port ${PORT}!`);
+//     // });
+// }
--- models/index.js
+++ models/index.js
@@ -1,18 +1,22 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
-
-const Page = db.define('page', {
-    title: Sequelize.STRING,
-    slug: Sequelize.STRING,
-    content: Sequelize.TEXT,
-    status: Sequelize.ENUM('open', 'closed')
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {
+  logging: false,
 });
 
-const User = db.define('user', {
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-})
+const Page = db.define("page", {
+  title: Sequelize.STRING,
+  slug: Sequelize.STRING,
+  content: Sequelize.TEXT,
+  status: Sequelize.ENUM("open", "closed"),
+});
+
+const User = db.define("user", {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
 
 module.exports = {
-  db
-}
+  db,
+  Page,
+  User,
+};
